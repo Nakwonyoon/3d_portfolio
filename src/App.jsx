bdb001b3058337fba4582e61c148411c2ab99467
윤낙원낙원 <sexy@MacBook-Pro.local>
2024-02-11 15:02:03 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="flex justify-between">
+        <div className="text-3xl font-bold underline">asdjkladsjklajdl
+        <p>asdjklasdjkl</p></div>
+      </header>
+    </>
+  )
+}
+
+export default App
