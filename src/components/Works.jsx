802af352590c5e78dd2ae3fa48c31b910e82488f
윤낙원낙원 <sexy@MacBook-Pro.local>
2024-02-13 09:40:54 +0900

    adding and styling ball

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { Tilt } from "react-tilt";
+
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  name,
+  description,
+  index,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      Test
+    </motion.div>
+  );
+};
 
 const Works = () => {
   return (
-    <div>Works</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}> MY WORK </p>
+        <h2 className={styles.sectionHeadText}> Projects. </h2>
+      </motion.div>
 
-export default Works
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 max-w-3xl leading-[30px] text-secondary text-[17px]"
+        >
+          Following projects showcase my skills and experience through
+          real-world examples of my work.
+        </motion.p>
+
+        <div className="mt-20 flex flex-wrap gap-7">
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,6 @@
-import React, { Suspense } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { useEffect, useState } from "react";
 
 import {
   Decal,
@@ -10,7 +11,7 @@ import {
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import { AmbientLight } from "three";
+import { MeshStandardMaterial } from "three";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -20,13 +21,38 @@ const Ball = (props) => {
       <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronBufferGeometry args={[1, 1]} />
+        {/* <icosahedronBufferGeometry args={[0, 0]} /> */}
+        {/* <MeshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        /> */}
+
+        {/* <Decal 
+        map={decal} 
+        position={[0, 0, 1]} 
+        rotation= {[2 * Math.PI, 0, 6.25]}
+        flatShading
+        /> */}
       </mesh>
     </Float>
   );
 };
 
 const BallCanvas = ({ icon }) => {
+  // const [shouldRender, setShouldRender] = useState(false);
+
+  // useEffect(() => {
+  //   // 컴포넌트 로딩을 5초 후로 지연
+  //   const timer = setTimeout(() => setShouldRender(true), 5000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  // if (!shouldRender) {
+  //   // 로딩 상태 또는 플레이스홀더를 여기에 표시할 수 있습니다.
+  //   return <div>Loading Ball...</div>;
+  // }
   return (
     <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
