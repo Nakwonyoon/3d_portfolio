3f407fc6ea3d651b55ac9d92f50c5910163b2483
nakwonyoon <yoonnw1000@gmail.com>
2024-02-13 10:10:58 +0900

    configure and fix bug for 3d balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 import {
   Decal,
@@ -11,7 +11,6 @@ import {
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import { MeshStandardMaterial } from "three";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
@@ -21,40 +20,32 @@ const Ball = (props) => {
       <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        {/* <icosahedronBufferGeometry args={[0, 0]} /> */}
-        {/* <MeshStandardMaterial
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
           color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
-        /> */}
-
-        {/* <Decal 
-        map={decal} 
-        position={[0, 0, 1]} 
-        rotation= {[2 * Math.PI, 0, 6.25]}
-        flatShading
-        /> */}
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
       </mesh>
     </Float>
   );
 };
 
 const BallCanvas = ({ icon }) => {
-  // const [shouldRender, setShouldRender] = useState(false);
-
-  // useEffect(() => {
-  //   // 컴포넌트 로딩을 5초 후로 지연
-  //   const timer = setTimeout(() => setShouldRender(true), 5000);
-  //   return () => clearTimeout(timer);
-  // }, []);
-
-  // if (!shouldRender) {
-  //   // 로딩 상태 또는 플레이스홀더를 여기에 표시할 수 있습니다.
-  //   return <div>Loading Ball...</div>;
-  // }
   return (
-    <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas
+      frameLoop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
